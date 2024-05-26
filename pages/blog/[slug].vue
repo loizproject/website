d030cc6232fea27ec591371716b114a7761d57c5
Emmanuel Ejei-Okeke <ejeiokekeemmanuel@gmail.com>
2024-05-26 02:40:26 +0100

    Blog pages

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,4 +1,6 @@
 <script setup>
+import { convert } from "html-to-text";
+
 const route = useRoute();
 const slug = route.params.slug;
 
@@ -37,36 +39,109 @@ const query = `
   }
 `;
 
-const article = ref(null);
-async function fetchArticle() {
-  try {
-    const graphqlQuery = {
-      query: query,
-      variables: {},
-    };
-    const { data } = await useAxiosPost(CMS_BASE_URL, graphqlQuery);
-    article.value = data.data.post;
-  } catch (error) {
-    console.warn("Failed to fetch article: An error occoured");
-    console.error(error);
-  }
-}
+const graphqlQuery = {
+  query: query,
+  variables: {},
+};
+const article = (await useAxiosPost(CMS_BASE_URL, graphqlQuery)).data.data.post;
+const { title, excerpt, categories, tags, date, content, featuredImage, author } =
+  article || {};
+const link = `${config.public.APP_URL}/resources/articles/${slug}`;
 
-onMounted(() => {
-  setTimeout(async () => {
-    await fetchArticle();
-    console.log(article.value);
-  }, 200);
+const meta = ref({
+  title: title,
+  description: convert(excerpt, { wordwrap: 120 }),
+  image: featuredImage?.node.sourceUrl,
+});
+
+useSeoMeta({
+  title: meta.value.title,
+  ogTitle: meta.value.title,
+  twitterTitle: meta.value.title,
+  description: meta.value.description,
+  ogDescription: meta.value.description,
+  twitterDescription: meta.value.description,
+  ogImage: meta.value.image,
+  twitterImage: meta.value.image,
+  twitterCard: "summary_large_image",
 });
 </script>
 
 <template>
   <div class="container">
-    <div class="my-5">
-      <h2 class="active-header">{{ article.title }}</h2>
+    <div v-if="!article">
+      <div class="d-flex align-center justify-center">
+        <img src="/svg/loading.svg" alt="" class="tw-w-28 my-10" />
+      </div>
     </div>
-    <div v-html="article.content" class="mt-2"></div>
+    <div v-else class="lg:tw-w-[90%] tw-mx-auto">
+      <h1 class="tw-text-5xl tw-font-black active-header my-5">
+        {{ title }}
+      </h1>
+      <img
+        :src="featuredImage?.node.sourceUrl"
+        :alt="title"
+        class="tw-rounded-lg tw-w-full"
+      />
+      <div class="bg-white pa-6 my-5 tw-border-[0.1px] tw-rounded-xl">
+        <div v-html="content" class="mt-2"></div>
+      </div>
+      <div class="tw-flex tw-items-center tw-text-4xl">
+        <div class="tw-flex tw-items-center">
+          <p class="mb-0 mr-2 tw-text-lg tw-font-bold">Share Post:</p>
+          <a
+            :href="`https://twitter.com/intent/tweet?original_referer=${encodeURI(
+              config.public.APP_URL
+            )}&text=${encodeURI(title)}&url=${encodeURI(`\n${link}`)}`"
+            target="_blank"
+            class="mr-3 tw-text-2xl tw-text-[#55ACEE]"
+          >
+            <iconify-icon icon="bi:twitter" class=""></iconify-icon>
+          </a>
+          <a
+            :href="`https://www.facebook.com/sharer/sharer.php?u=${encodeURI(
+              link
+            )}&amp;src=sdkpreparse`"
+            target="_blank"
+            class="mr-3 tw-text-2xl tw-text-[#1C54D1]"
+          >
+            <iconify-icon icon="ic:baseline-facebook" class=""></iconify-icon>
+          </a>
+          <a
+            :href="`http://www.linkedin.com/shareArticle?mini=true&url=${link}&title=${encodeURI(
+              title
+            )}&summary=${encodeURI(meta.description)}`"
+            target="_blank"
+            class="mr-3 tw-text-2xl tw-text-[#0A66C2]"
+          >
+            <iconify-icon
+              icon="entypo-social:linkedin-with-circle"
+              class=""
+            ></iconify-icon>
+          </a>
+          <span
+            target="_blank"
+            class="mr-3 tw-text-2xl tw-cursor-pointer"
+            @click="useCopyText(link)"
+          >
+            <iconify-icon icon="ph:link-duotone"></iconify-icon>
+          </span>
+        </div>
+        <p v-if="article.date" class="tw-text-sm tw-absolute tw-bottom-5 tw-left-5 mb-0">
+          {{ useFormatDay(article.date) }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.active-header {
+  color: transparent;
+  background: linear-gradient(90deg, #13abcf 0%, #eb0c8f 100.01%);
+  background-clip: text;
+  text-transform: uppercase;
+  width: fit-content;
+  font-family: trip-bold, sans-serif;
+}
+</style>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,6 +2,9 @@
 const config = useRuntimeConfig();
 const CMS_BASE_URL = config.public.CMS_BASE_URL;
 
+const loading = ref(true);
+const search = ref("");
+
 const query = `
 {
   posts(first: 100) {
@@ -52,29 +55,79 @@ async function fetchArticles() {
     console.error(error);
   }
 }
+const currentShowingArticles = computed(() => {
+  let res = [];
+  articles.value.forEach((item) => {
+    if (
+      item.node.title?.toUpperCase().includes(search.value.toUpperCase()) ||
+      item.node.excerpt?.toUpperCase().includes(search.value.toUpperCase())
+    ) {
+      res.push(item);
+    }
+  });
+  return res;
+});
 
 onMounted(() => {
   setTimeout(async () => {
     await fetchArticles();
-    console.log(articles.value);
+    loading.value = false;
   }, 200);
 });
 </script>
 
 <template>
-  <div class="container">
-    <div class="my-5">
-      <h2 class="active-header">Loiz Blog</h2>
-      <p>Take a look at our articles & publications</p>
+  <div id="blog" class="container">
+    <div class="tw-flex tw-items-center">
+      <div class="my-5">
+        <h2 class="active-header">Loiz Blog</h2>
+        <p>Take a look at our articles & publications</p>
+      </div>
+      <div class="tw-flex-grow"></div>
+      <div class="tw-w-1/4">
+        <v-text-field
+          v-model="search"
+          variant="outlined"
+          flat
+          clearable
+          placeholder="Search"
+          prepend-inner-icon="mdi-magnify"
+          density="comfortable"
+        >
+        </v-text-field>
+      </div>
+    </div>
+    <div v-if="loading">
+      <div class="d-flex align-center justify-center">
+        <img src="/svg/loading.svg" alt="" class="tw-w-28 my-10" />
+      </div>
     </div>
     <div
-      class="tw-grid tw-grid-cols-1 md:tw-grid-cols-2 lg:tw-grid-cols-3 2xl:tw-grid-cols-4 tw-gap-4"
+      v-else
+      class="tw-grid tw-grid-cols-1 md:tw-grid-cols-2 lg:tw-grid-cols-3 2xl:tw-grid-cols-4 tw-gap-4 tw-animate-show"
     >
-      <nuxt-link v-for="n in articles" :key="n.node.id" :to="`/blog/${n.node.slug}`">
-        <v-card variant="flat" class="pa-5 tw-rounded-xl">
-          <h3 class="tw-text-xl lg:tw-text-2xl">{{ n.node.title }}</h3>
-          <div v-html="n.node.excerpt" class="mt-2"></div>
-        </v-card>
+      <nuxt-link
+        v-for="n in currentShowingArticles"
+        :key="n.node.id"
+        :to="`/blog/${n.node.slug}`"
+        class="bg-white tw-rounded-lg tw-relative"
+      >
+        <img
+          :src="n.node.featuredImage?.node.sourceUrl"
+          :alt="n.node.title"
+          class="tw-rounded-t-lg tw-h-56 tw-w-full tw-object-cover"
+        />
+        <div class="pa-5">
+          <h3 class="tw-text-lg 2xl:tw-text-xl">{{ n.node.title }}</h3>
+          <div
+            v-html="n.node.excerpt"
+            id="main-contentt"
+            class="main-contentt tw-text-sm mt-2 pb-5"
+          ></div>
+          <p v-if="n.node.date" class="tw-text-sm tw-absolute tw-bottom-5 tw-left-5 mb-0">
+            {{ useFormatDay(n.node.date) }}
+          </p>
+        </div>
       </nuxt-link>
     </div>
   </div>
@@ -98,11 +151,6 @@ h2 {
     background-clip: text;
     text-transform: uppercase;
     width: fit-content;
-  }
-}
-.v-card {
-  p {
-    font-size: 16px !important;
   }
 }
 </style>
