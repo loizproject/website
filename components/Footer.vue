5625158985f3deab3d573aabcdf6a65c9aea2f99
Dayo Akinyemi <81328621+Dee-Dayo@users.noreply.github.com>
2025-07-16 17:34:17 +0100

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { useDisplay } from "vuetify";
-import { useStore } from "~/store";
+import {useDisplay} from "vuetify";
+import {useStore} from "~/store";
 import {
   footerSect1,
   footerSect2,
@@ -13,7 +13,7 @@ import {
 } from "~/utils/site-content/site-layout.json";
 
 const store = useStore();
-const { smAndDown } = useDisplay();
+const {smAndDown} = useDisplay();
 
 const cookies = computed(() => store.cookies);
 
@@ -27,9 +27,9 @@ const subscribeNewsletter = async () => {
   const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   try {
     if (!newsletterForm.value.email) {
-      store.setToast("Please enter your email", { type: "error" });
+      store.setToast("Please enter your email", {type: "error"});
     } else if (!pattern.test(newsletterForm.value.email)) {
-      store.setToast("Please enter a valid email", { type: "error" });
+      store.setToast("Please enter a valid email", {type: "error"});
     } else {
       await useAxiosPost("/newsletter/subscribe", newsletterForm.value);
       store.setToast("You have succesfully subscribed to our newsletter", {
@@ -46,7 +46,7 @@ const checkCookie = async () => {
   let cookie = useGetCookie("loiz_user_preferences");
   if (cookie) {
     try {
-      const { name, value } = cookie;
+      const {name, value} = cookie;
       // Set cookie key, form & modal
       cookieKey.value = name;
       cookieForm.value = JSON.parse(value);
@@ -116,7 +116,7 @@ onMounted(() => {
   <section id="footer" class="footer mt-10">
     <v-container>
       <nuxt-link to="/" class="logo">
-        <nuxt-img src="/svg/logo.svg" width="140px" />
+        <nuxt-img src="/svg/logo.svg" width="140px"/>
       </nuxt-link>
       <v-row class="mt-4" no-gutters align="start">
         <v-col cols="12">
@@ -126,10 +126,27 @@ onMounted(() => {
               <div v-for="item in footerSect1" :key="item.name" class="footer__list">
                 <nuxt-link :to="item.route">{{ item.name }}</nuxt-link>
               </div>
-              <div class="footer__list">
-                <a @click="toggleManageCookies">Manage Cookie Preferences </a>
+
+              <div class="footer__actions mb-8 mt-5 mb-md-0 mt-md-8">
+                <h4>Subscribe to our Newsletter</h4>
+                <div class="newsletter mt-3">
+                  <v-form
+                      ref="newsletterForm"
+                      lazy-validation
+                      class="d-flex align-center"
+                      @submit.prevent="subscribeNewsletter"
+                  >
+                    <input
+                        v-model="newsletterForm.email"
+                        type="text"
+                        placeholder="Enter Email"
+                    />
+                    <v-btn type="submit" class="submit"> Subscribe</v-btn>
+                  </v-form>
+                </div>
               </div>
             </v-col>
+
             <v-col cols="6" md="4">
               <h3>Our Services</h3>
               <div v-for="item in footerSect2" :key="item.name" class="footer__list">
@@ -137,41 +154,37 @@ onMounted(() => {
               </div>
               <div class="footer__list footer__list--var">
                 <a
-                  href="https://drive.google.com/file/d/1vdQ9dHMe7luKT2Kz24luBnryZVin6-gI/view?usp=sharing"
-                  target="_blank"
+                    href="https://drive.google.com/file/d/1vdQ9dHMe7luKT2Kz24luBnryZVin6-gI/view?usp=sharing"
+                    target="_blank"
                 >
                   Booking Journey Guide
                 </a>
               </div>
             </v-col>
+
             <v-col cols="12" md="4">
               <h3>Third Party Services</h3>
               <div
-                v-for="item in thirdPartyServices"
-                :key="item.name"
-                class="footer__list"
+                  v-for="item in thirdPartyServices"
+                  :key="item.name"
+                  class="footer__list"
               >
                 <nuxt-link :to="item.route">{{ item.name }}</nuxt-link>
               </div>
-              <div
-                class="footer__actions mb-8 mt-5 mb-md-0 mt-md-8 d-flex d-sm-block flex-column justify-center align-center ml-auto"
-              >
-                <h4>Subscribe to our Newsletter</h4>
-                <!-- <p>Subscribe to get information about our services and events</p> -->
-                <div class="newsletter mt-3">
-                  <v-form
-                    ref="newsletterForm"
-                    lazy-validation
-                    class="d-flex align-center"
-                    @submit.prevent="subscribeNewsletter"
-                  >
-                    <input
-                      v-model="newsletterForm.email"
-                      type="text"
-                      placeholder="Enter Email"
-                    />
-                    <v-btn type="submit" class="submit"> Subscribe </v-btn>
-                  </v-form>
+
+              <div class="mt-8">
+                <h3>Compliance</h3>
+                <div class="footer__list">
+                  <nuxt-link to="/data-privacy">Data Privacy Notice</nuxt-link>
+                </div>
+                <div class="footer__list">
+                  <nuxt-link to="/privacy-policy">Privacy Policy</nuxt-link>
+                </div>
+                <div class="footer__list">
+                  <nuxt-link to="/cookies-policy">Cookies Policy</nuxt-link>
+                </div>
+                <div class="footer__list">
+                  <a @click="toggleManageCookies">Manage Cookie Preferences</a>
                 </div>
               </div>
             </v-col>
@@ -181,10 +194,10 @@ onMounted(() => {
       <v-divider class="line mt-8 mb-4"></v-divider>
       <div class="partners d-flex align-center justify-center">
         <nuxt-img
-          v-for="item in partners"
-          :key="item.name"
-          :src="item.src"
-          class="partners__img mx-2"
+            v-for="item in partners"
+            :key="item.name"
+            :src="item.src"
+            class="partners__img mx-2"
         ></nuxt-img>
       </div>
       <v-divider class="line my-4"></v-divider>
@@ -204,9 +217,9 @@ onMounted(() => {
         <div class="d-flex align-center justify-space-between">
           <h2>Cookies and Privacy</h2>
           <iconify-icon
-            icon="ant-design:close-circle"
-            class="cookies__icon"
-            @click="$store.commit('setCookies', true)"
+              icon="ant-design:close-circle"
+              class="cookies__icon"
+              @click="$store.commit('setCookies', true)"
           ></iconify-icon>
         </div>
         <p>
@@ -218,44 +231,47 @@ onMounted(() => {
         </p>
         <div class="d-flex align-center">
           <v-btn
-            class="cookies__btn"
-            elevation="0"
-            color="#e7028e"
-            @click="acceptAllCookies"
-            >Accept All</v-btn
+              class="cookies__btn"
+              elevation="0"
+              color="#e7028e"
+              @click="acceptAllCookies"
+          >Accept All
+          </v-btn
           >
           <v-btn
-            class="cookies__btn mx-4"
-            elevation="0"
-            color="#e7028e"
-            @click="rejectAllCookies"
-            >Reject All</v-btn
+              class="cookies__btn mx-4"
+              elevation="0"
+              color="#e7028e"
+              @click="rejectAllCookies"
+          >Reject All
+          </v-btn
           >
           <v-btn
-            class="cookies__btn cookies__btn--manage"
-            elevation="0"
-            color="#e7028e"
-            variant="text"
-            @click="toggleManageCookies"
-            >Manage Cookies</v-btn
+              class="cookies__btn cookies__btn--manage"
+              elevation="0"
+              color="#e7028e"
+              variant="text"
+              @click="toggleManageCookies"
+          >Manage Cookies
+          </v-btn
           >
         </div>
       </v-container>
     </section>
     <v-dialog
-      v-model="manageCookiesModal"
-      content-class="cookies cookies--manage"
-      width="80%"
-      max-width="550px"
-      persistent
+        v-model="manageCookiesModal"
+        content-class="cookies cookies--manage"
+        width="80%"
+        max-width="550px"
+        persistent
     >
       <v-card>
         <div class="d-flex align-center justify-space-between mb-5">
           <h2>Manage Cookies</h2>
           <iconify-icon
-            icon="ant-design:close-circle"
-            class="cookies__icon"
-            @click="toggleManageCookies"
+              icon="ant-design:close-circle"
+              class="cookies__icon"
+              @click="toggleManageCookies"
           ></iconify-icon>
         </div>
         <p>
@@ -274,9 +290,9 @@ onMounted(() => {
             <div class="content-preferences__field">Performance Cookies</div>
             <div>
               <v-switch
-                v-model="cookieForm.performance"
-                hide-details
-                color="#e7028e"
+                  v-model="cookieForm.performance"
+                  hide-details
+                  color="#e7028e"
               ></v-switch>
             </div>
           </div>
@@ -285,9 +301,9 @@ onMounted(() => {
             <div class="content-preferences__field">Functional cookies</div>
             <div>
               <v-switch
-                v-model="cookieForm.functional"
-                hide-details
-                color="#e7028e"
+                  v-model="cookieForm.functional"
+                  hide-details
+                  color="#e7028e"
               ></v-switch>
             </div>
           </div>
@@ -296,9 +312,9 @@ onMounted(() => {
             <div class="content-preferences__field">Targeting cookies</div>
             <div>
               <v-switch
-                v-model="cookieForm.targeting"
-                hide-details
-                color="#e7028e"
+                  v-model="cookieForm.targeting"
+                  hide-details
+                  color="#e7028e"
               ></v-switch>
             </div>
           </div>
@@ -306,28 +322,31 @@ onMounted(() => {
         </div>
         <div class="d-flex align-center justify-center justify-sm-start mt-5">
           <v-btn
-            class="cookies__btn"
-            elevation="0"
-            color="#e7028e"
-            :small="smAndDown"
-            @click="acceptAllCookies"
-            >Allow All</v-btn
+              class="cookies__btn"
+              elevation="0"
+              color="#e7028e"
+              :small="smAndDown"
+              @click="acceptAllCookies"
+          >Allow All
+          </v-btn
           >
           <v-btn
-            class="cookies__btn mx-2 mx-md-4"
-            elevation="0"
-            color="#e7028e"
-            :small="smAndDown"
-            @click="rejectAllCookies"
-            >Reject All</v-btn
+              class="cookies__btn mx-2 mx-md-4"
+              elevation="0"
+              color="#e7028e"
+              :small="smAndDown"
+              @click="rejectAllCookies"
+          >Reject All
+          </v-btn
           >
           <v-btn
-            class="cookies__btn"
-            elevation="0"
-            color="#e7028e"
-            :small="smAndDown"
-            @click="confirmCookieChoice"
-            >Confirm My Choice</v-btn
+              class="cookies__btn"
+              elevation="0"
+              color="#e7028e"
+              :small="smAndDown"
+              @click="confirmCookieChoice"
+          >Confirm My Choice
+          </v-btn
           >
         </div>
       </v-card>
@@ -409,11 +428,13 @@ onMounted(() => {
     // margin-right: auto;
   }
 }
+
 .partners {
   &__img {
     width: 70px;
   }
 }
+
 .newsletter {
   & input {
     width: 100%;
@@ -436,6 +457,7 @@ onMounted(() => {
     height: 40px;
   }
 }
+
 .cookies {
   position: fixed;
   bottom: 0;
